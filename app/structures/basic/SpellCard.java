bce761e0b2bbde40024746fde6be08983fd74c3a
Bobby Ayvazov <2066244a@student.gla.ac.uk>
2023-02-08 20:29:40 +0000

    SpellCard class added

---
+++ app/structures/basic/SpellCard.java
@@ -0,0 +1,14 @@
+package structures.basic;
+
+public class SpellCard extends Card{
+	
+	
+	public SpellCard(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard) {
+		super(id, cardname, manacost, miniCard, bigCard);
+	}
+	
+	private void ability() {
+		// TO DO include code here
+	}
+
+}
