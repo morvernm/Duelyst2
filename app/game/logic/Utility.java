2735cbafcfbbaf5418a270092c3e7dc06cbd118e
Bobby Ayvazov <2066244a@student.gla.ac.uk>
2023-02-10 22:03:59 +0000

    DetermineTargets() now determines targets based on if unit has moved/attacked and if is yet to move too

--- app/game/logic/Utility.java
+++ app/game/logic/Utility.java
@@ -2,6 +2,7 @@ package game.logic;
 
 import java.util.ArrayList;
 
+import commands.BasicCommands;
 import structures.basic.Player;
 import structures.basic.Tile;
 import structures.basic.Unit;
@@ -12,25 +13,43 @@ public class Utility {
 	 * This class is the utility class where methods with some main logic of the game will be provided
 	 * 
 	 */
+	
+	public static ArrayList<Tile> determineTargets(Tile tile, ArrayList<Tile> positions, Player enemy, Tile[][] board){
+		
+		ArrayList<Tile> validAttacks = new ArrayList<>();
+		
+		// Has Attacked already
+		if (tile.getOccupier().hasAttacked()) {
+			return null;
+		
+		// Has moved but has not attacked - consider only the current position
+		} else if (tile.getOccupier().hasMoved() && !tile.getOccupier().hasAttacked()) {
+			return getValidTargets(tile, enemy, board);
+		
+		// Has not moved nor attacked - consider all possible movements as well. 
+		} else if (!tile.getOccupier().hasMoved() && !tile.getOccupier().hasAttacked()) {
+			System.out.println("has NOT moved NOR attacked");
+			for (Tile position : positions) {
+				validAttacks.addAll(getValidTargets(position,enemy,board));
+			}
+		}
+		return validAttacks;
+		
+	}
+	
 	public static ArrayList<Tile> getValidTargets(Tile tile, Player enemy, Tile[][] board){
 		
 		ArrayList<Tile> validAttacks = new ArrayList<>();
 		
 		for (Unit unit : enemy.getUnits()) {
-			System.out.println("Position of enemy is: " + unit.getPosition().getTilex() + " " + unit.getPosition().getTiley());
 			int unitx = unit.getPosition().getTilex();
 			int unity = unit.getPosition().getTiley();
 			
-						
-			if (unitx - tile.getTilex() < 2 && unity - tile.getTiley() < 2 ) {
+			if (Math.abs(unitx - tile.getTilex()) < 2 && Math.abs(unity- tile.getTiley()) < 2 ) {
 				validAttacks.add(board[unitx][unity]);
 			}
 		}
-		for (Tile t : validAttacks) {
-			System.out.println("Enemy on " + t.getTilex() + " " + t.getTiley());
-		}
 		return validAttacks;
-		
 	}
 	
 	
