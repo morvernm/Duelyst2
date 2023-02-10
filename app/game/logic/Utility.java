e27cf39e6d270126900a2b5c0c4188ee4700e928
Bobby Ayvazov <2066244a@student.gla.ac.uk>
2023-02-10 16:26:41 +0000

    Cherry picking changes of Tile from other branch

--- app/game/logic/Utility.java
+++ app/game/logic/Utility.java
@@ -1,5 +1,28 @@
 package game.logic;
 
-public class Utility {
+import java.util.ArrayList;
 
+import structures.basic.Player;
+import structures.basic.Tile;
+import structures.basic.Unit;
+
+
+public class Utility {
+	/*
+	 * This class is the utility class where methods with some main logic of the game will be provided
+	 * 
+	 */
+	public static ArrayList<Tile> getValidATargets(Tile tile, Player enemy){
+		
+		ArrayList<Tile> validAttacks = new ArrayList<>();
+		
+		
+		
+		
+		return validAttacks;
+		
+	}
+	
+	
+	
 }
