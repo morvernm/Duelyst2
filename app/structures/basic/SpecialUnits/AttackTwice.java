aa951b9657e007cd772960e5d2e5b2578b1e88a3
Morvern Mackintosh <2809522m@student.gla.ac.uk>
2023-02-27 12:52:49 +0000

    Added abstract AttackTwice class for Serpenti and AzuriteLion units

---
+++ app/structures/basic/SpecialUnits/AttackTwice.java
@@ -0,0 +1,29 @@
+package structures.basic.SpecialUnits;
+
+import structures.basic.Unit; 
+/*
+ * This class represents the units that can attack twice per turn 
+ */
+public abstract class AttackTwice extends Unit {
+	private int attackCount = 0;
+	private boolean attacked;
+	
+	public int getAttackCount() {
+		return attackCount;
+	}
+	
+//	overriding Unit methods
+	
+	public void setAttacked() {
+		attacked = false; 
+		attackCount+=1; //increase attack count after each attack
+	}
+	public boolean hasAttacked() {
+		if(attackCount == 2) { //once unit has attacked twice, set attacked to true
+			attacked = true;
+		}
+		return this.attacked;
+	}
+
+
+}
--- app/structures/basic/SpecialUnits/AzuriteLion.java
+++ app/structures/basic/SpecialUnits/AzuriteLion.java
@@ -1,31 +1,12 @@
 package structures.basic.SpecialUnits;
 
-import akka.actor.ActorRef;
-import commands.BasicCommands;
-import game.logic.Gui;
-import game.logic.Utility;
-import structures.GameState;
 import structures.basic.Unit;
 
-public class AzuriteLion extends Unit{
-	private final String name = "AzuriteLion";
-	private int attackCount = 0;
-	private boolean attacked;
+/*
+ *  This class represents the AzuriteLion unit's attack twice per turn ability and extends the AttackTwice class
+ */
 
-	public int getAttackCount() {
-		return attackCount;
-	}
-	public void setAttacked() {
-		attacked = false;
-		attackCount+=1;
-	}
-	public boolean hasAttacked() {
-		if(attackCount == 2) {
-			attacked = true;
-		}
-		return this.attacked;
-	}
-
-	
+public class AzuriteLion extends AttackTwice{
+	private final String name = "AzuriteLion";	
 
 }
