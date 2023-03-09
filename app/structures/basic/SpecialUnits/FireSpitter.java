ba5aca94911747b859d7a2e04f1d910828327f67
Greta Gavsinaite <2207573G@student.gla.ac.uk>
2023-03-09 21:01:27 +0000

    added comments to some Units, Gui, MoveAction and AttackAction classes where appropriate

--- app/structures/basic/SpecialUnits/FireSpitter.java
+++ app/structures/basic/SpecialUnits/FireSpitter.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * FireSpitter extends RangedAttack - can attack any enemy unit on the board
+ */
 public class FireSpitter extends RangedAttack {
 
     private final String name = "Fire Spitter";
